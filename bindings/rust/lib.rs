b7df9dae90e92d84a09eb9d74acba58027598f4c
Benjamin Bannier <bbannier@gmail.com>
2024-12-16 14:33:43 +0100

    Migrate to `tree-sitter-language`

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -7,7 +7,8 @@
 //! let code = r#"
 //! "#;
 //! let mut parser = tree_sitter::Parser::new();
-//! parser.set_language(&tree_sitter_spicy::language()).expect("Error loading Spicy grammar");
+//! let language = tree_sitter_spicy::LANGUAGE;
+//! parser.set_language(&language.into()).expect("Error loading Spicy grammar");
 //! let tree = parser.parse(code, None).unwrap();
 //! assert!(!tree.root_node().has_error());
 //! ```
@@ -17,18 +18,14 @@
 //! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
 //! [tree-sitter]: https://tree-sitter.github.io/
 
-use tree_sitter::Language;
+use tree_sitter_language::LanguageFn;
 
 extern "C" {
-    fn tree_sitter_spicy() -> Language;
+    fn tree_sitter_spicy() -> *const ();
 }
 
-/// Get the tree-sitter [Language][] for this grammar.
-///
-/// [Language]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Language.html
-pub fn language() -> Language {
-    unsafe { tree_sitter_spicy() }
-}
+/// The tree-sitter [`LanguageFn`] for this grammar.
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_spicy) };
 
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
@@ -48,7 +45,7 @@ mod tests {
     fn test_can_load_grammar() {
         let mut parser = tree_sitter::Parser::new();
         parser
-            .set_language(&super::language())
+            .set_language(&super::LANGUAGE.into())
             .expect("Error loading Spicy grammar");
     }
 }
