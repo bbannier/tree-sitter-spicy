a29b2a1bdc13808a42386100e56c3e6a0ee40a42
Benjamin Bannier <bbannier@gmail.com>
2025-07-07 18:46:16 +0200

    Add injection for printf strings

--- queries/injections.scm
+++ queries/injections.scm
@@ -2,3 +2,9 @@
  (regexp_pattern) @injection.content
  (#set! injection.language "regex")
 )
+
+(
+ (string) @injection.content
+ (#match? @injection.content "%")
+ (#set! injection.language "printf")
+)
